85434c4d76fc3c32078848fa635491a4e4df992a
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-05-12 19:33:43 +0530

    adding comments for better understanding

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -14,31 +14,35 @@ import (
 )
 
 func main() {
-	// Load config
-
+	// Load configuration --------------------------------------------------------------------------------
 	cfg := config.MustLoad()
 
-	// Custom Logger or Inbuilt Logger
 	// Connect to DB
-	// Setup router
+	// Setup router ---------------------------------------------------------------------------------------
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to the Students API"))
 	})
 
-	// Setup server
+	// Setup server --------------------------------------------------------------------------------------
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
+	// Setup logging -------------------------------------------------------------------------------------
+	// Slog is a structured logger for Go
 	slog.Info("Server started on", slog.String("Address", cfg.Addr))
 
+	// Setup graceful shutdown ---------------------------------------------------------------------------
+	// Create a channel to listen for OS signals & this will allow us to gracefully shutdown the server --
 	done := make(chan os.Signal, 1)
 
+	// Notify the channel when an interrupt or terminate signal is received -----------------------------
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
+	// Start the server in a goroutine ------------------------------------------------------------------
 	go func() {
 		err := server.ListenAndServe()
 		if err != nil {
@@ -46,14 +50,20 @@ func main() {
 		}
 	}()
 
+	// Wait for a signal to shutdown the server ----------------------------------------------------------
 	<-done
 
 	slog.Info("Shutting down server...")
 
+	// Create a context with a timeout for the shutdown process ------------------------------------------
+	// This will allow us to wait for a maximum of 5 seconds for the server to shutdown ------------------
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
+	// Ensure the cancel function is called to release resources -----------------------------------------
+	// This is important to avoid memory leaks and to ensure that the context is properly cleaned up -----
 	defer cancel()
 
+	// Shutdown the server & this will close all connections and stop the server from accepting new requests
 	err := server.Shutdown(ctx)
 
 	if err != nil {
